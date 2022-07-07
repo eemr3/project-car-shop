7305328d2c23307335f1f4acba0701874920896e
eemr3 <eemr3@yahoo.com.br>
2022-07-07 17:49:05 -0300

    refactor: interfaces/CarInterface.ts
    adiciona o export a constante CarSchema

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { Vehicle } from './VehicleInterface';
 
-const CarSchema = z.object({
-  doorsQty: z.number().lte(2).gte(4),
-  seatsQty: z.number().lte(2).gte(7),
+export const CarSchema = z.object({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
 });
 
 export type Car = z.infer<typeof CarSchema> & Vehicle;
