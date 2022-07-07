dd7cd3e4c7baaae20abef47f9161d9128e6aacb6
eemr3 <eemr3@yahoo.com.br>
2022-07-07 17:46:49 -0300

    feat: :sparkles: Model.generic.ts
    cria a model generica da palicação

---
+++ src/models/Model.generic.ts
@@ -0,0 +1,34 @@
+import { Model as M } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class ModelGeneric<T> implements Model<T> {
+  constructor(protected modelMongoose: M<T>) {
+    
+  }
+
+  async create(entity: T): Promise<T> {
+    return this.modelMongoose.create(entity);
+  }
+
+  async read(): Promise<T[]> {
+    return this.modelMongoose.find();
+  }
+
+  async readOne(id: string): Promise<T | null> {
+    return this.modelMongoose.findOne({ _id: id });
+  }
+
+  async update(id: string, entity: T): Promise<T | null> {
+    return this.modelMongoose.findOneAndUpdate(
+      { _id: id }, 
+      entity,
+      { returnOriginal: false },
+    );
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this.modelMongoose.findOneAndDelete({ _id: id });
+  }
+}
+
+export default ModelGeneric;
