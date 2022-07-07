174dac6686d61cd13f526398b29882fd2146f013
eemr3 <eemr3@yahoo.com.br>
2022-07-07 17:42:48 -0300

    feat: :sparkles: service.generic.ts
    cria a service generica da aplicação

---
+++ src/services/Service.generic.ts
@@ -0,0 +1,28 @@
+import { Model } from '../interfaces/ModelInterface';
+import { IServiceError } from './interfaces/error.interface';
+
+abstract class ServiceGeneric<T> {
+  constructor(protected model: Model<T>) {}
+
+  async create(entity: T): Promise<T | IServiceError> {
+    return this.model.create(entity);
+  }
+
+  async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  async readOne(id: string): Promise<T | null | IServiceError> {
+    return this.model.readOne(id);
+  }
+
+  async update(id: string, entity: T): Promise<T | null | IServiceError> {
+    return this.model.update(id, entity);
+  }
+
+  async delete(id: string): Promise<T | null | IServiceError> {
+    return this.model.delete(id);
+  }
+}
+
+export default ServiceGeneric;
