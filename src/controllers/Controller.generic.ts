0eb8ab564faf7d9a3632178ffc1e01b9f1e8958e
eemr3 <eemr3@yahoo.com.br>
2022-07-07 17:39:07 -0300

    feat: :sparkles: controller.generic.ts
    cria controller generico para aplicação

---
+++ src/controllers/Controller.generic.ts
@@ -0,0 +1,42 @@
+import { Response } from 'express';
+import ServiceGeneric from '../services/Service.generic';
+// import { Model } from '../interfaces/ModelInterface';
+
+import { IRequestWithBody } from './interfaces/Controller.interface';
+
+export type ResponseError = {
+  error: unknown
+};
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+abstract class ControllerGeneric<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: ServiceGeneric<T | ResponseError>) {}
+
+  abstract create(
+    req: IRequestWithBody<T>, res: Response): Promise<typeof res>;
+
+  abstract read(
+    req: IRequestWithBody<T>,
+    res: Response<T | ResponseError>): Promise<typeof res>;
+
+  abstract readOne(req: IRequestWithBody<T | ResponseError>, res: Response):
+  Promise<typeof res>;
+
+  abstract update(req: IRequestWithBody<T | ResponseError>,
+    res: Response): Promise<typeof res>;
+
+  abstract delete(req: IRequestWithBody<T | ResponseError>,
+    res: Response): Promise<typeof res>;
+}
+
+export default ControllerGeneric;
