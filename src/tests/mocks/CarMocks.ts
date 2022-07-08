f92c8a0b6cd2e04381a10cc9f55822bff2a28beb
eemr3 <eemr3@yahoo.com.br>
2022-07-08 16:43:11 -0300

    feat: CarMocks.ts
    cria arquivo que contem mocks do retorno das rotas para tests

---
+++ src/tests/mocks/CarMocks.ts
@@ -0,0 +1,49 @@
+export const bodyCar = {
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+export const returnCreateCar = {
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  doorsQty: 2,
+  seatsQty: 2,
+  _id: '62c87efaf1f9d111bf4b6acc',
+};
+
+export const returnFindAllCars = [
+  {
+    _id: '62c87efaf1f9d111bf4b6acc',
+    model: 'Ferrari Maranello',
+    year: 1963,
+    color: 'red',
+    buyValue: 3500000,
+    doorsQty: 2,
+    seatsQty: 2,
+  },
+];
+
+export const bodyUpdate = {
+  model: 'Fiat Uno',
+  year: 1963,
+  color: 'blue',
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4,
+};
+
+export const returnUpdateCar = {
+  _id: '62c87efaf1f9d111bf4b6acc',
+  model: 'Fiat Uno',
+  year: 1963,
+  color: 'blue',
+  buyValue: 3500,
+  doorsQty: 4,
+  seatsQty: 4,
+};
