97b2f5006dab2869fc398cb11b92a4a8c026c874
eemr3 <eemr3@yahoo.com.br>
2022-07-08 22:05:15 -0300

    feat: :sparkles: server.ts
    adiciona rota para Motorcycle

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,20 @@ import App from './app';
 import CarController from './controllers/Car.controller';
 
 import { Car } from './interfaces/CarInterface';
+import MortorcycleController from './controllers/Motorcycle.controller';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carController = new CarController();
+const motorcycleController = new MortorcycleController();
 
 const carRouter = new CustomRouter<Car>();
+const motocycleRouter = new CustomRouter<Motorcycle>();
 carRouter.addRoute(carController);
+motocycleRouter.addRoute(motorcycleController);
 
 server.addRouter(carRouter.router);
+server.addRouter(motocycleRouter.router);
 
 export default server;
