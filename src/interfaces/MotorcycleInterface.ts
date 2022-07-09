f718db2ca461bc65b30e6a0a0e0404fea5cd89a7
eemr3 <eemr3@yahoo.com.br>
2022-07-08 21:38:00 -0300

    feat: :sparkles: MotorcycleInterface.ts
    cria a itnerface para Motorcycle

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { Vehicle } from './VehicleInterface';
+
+const VALUES = ['Street', 'Custom', 'Trail'] as const;
+export const MotorcycleSchema = z.object({
+  category: z.enum(VALUES),
+  engineCapacity: z.number().lte(2500),
+});
+
+export type Motorcycle = z.infer<typeof MotorcycleSchema> & Vehicle;
