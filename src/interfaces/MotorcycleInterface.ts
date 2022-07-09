bfee4621c7c8f0dced1bb989f2eb907f16517b94
eemr3 <eemr3@yahoo.com.br>
2022-07-08 22:03:32 -0300

    feat: :sparkles: MotorcycleIncerface.ts
    cria a interface para Motorcycle

--- src/interfaces/MotorcycleInterface.ts
+++ src/interfaces/MotorcycleInterface.ts
@@ -4,7 +4,7 @@ import { Vehicle } from './VehicleInterface';
 const VALUES = ['Street', 'Custom', 'Trail'] as const;
 export const MotorcycleSchema = z.object({
   category: z.enum(VALUES),
-  engineCapacity: z.number().lte(2500),
+  engineCapacity: z.number().gte(1).lte(2500),
 });
 
 export type Motorcycle = z.infer<typeof MotorcycleSchema> & Vehicle;
